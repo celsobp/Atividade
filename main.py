1b3aeb7699a9e71cf247e2da745965963bd19779
celsobp <celsobp@gmail.com>
2024-09-13 22:32:20 -0300

    inclusão de testes de entrada

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # Rolando dados
 from random import randint
 
+lados = 0
+totalDados = 0
+
 print("Vamos rolar dados!")
-lados = input("Digite o número de lados dos dados (mínimo 6): ")
+while int(lados) < 6:
+    lados = input("Digite o número de lados dos dados (mínimo 6): ")
+
 print("\nTotal de lados do(s) dado(s): " + lados)
-totalDados = input("\nDigite o total de dados a rolar: ")
+while int(totalDados) < 1:
+    totalDados = input("\nDigite o total de dados a rolar (mínimo 1): ")
+
 print("\nTotal de dado(s) a rolar: " + totalDados)
 
 for x in range(0, int(totalDados)):
-    print("\n Valor do dado nº " + str(x+1) + " é " + str(randint(1, int(lados))))
+    print("\nValor do dado nº " + str(x+1) + " é " + str(randint(1, int(lados))))
