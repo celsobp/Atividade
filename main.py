e8dac1a93896eeb962150f0cf6f220bb5aeb0118
celsobp <celsobp@gmail.com>
2024-09-30 20:18:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 # Rolando dados
 from random import randint
 
-lados = 0
-totalDados = 0
-soma = 0
+continua = True
 
-print("Vamos rolar dados!")
-while int(lados) < 6:
-    lados = input("Digite o número de lados dos dados (mínimo 6): ")
+while (continua) == True :
 
-print("\nTotal de lados do(s) dado(s): " + lados)
-while int(totalDados) < 1:
-    totalDados = input("\nDigite o total de dados a rolar (mínimo 1): ")
+    lados = 0
+    totalDados = 0
+    soma = 0
+    pergunta = ""
 
-print("\nTotal de dado(s) a rolar: " + totalDados)
+    print("Vamos rolar dados!")
+    while int(lados) < 6:
+        lados = input("Digite o número de lados dos dados (mínimo 6): ")
 
-for x in range(0, int(totalDados)):
-    valor = randint(1, int(lados))
-    print("\nValor do dado nº " + str(x+1) + " é " + str(valor))
-    soma = soma + valor
+    print("\nTotal de lados do(s) dado(s): " + lados)
+    while int(totalDados) < 1:
+        totalDados = input("\nDigite o total de dados a rolar (mínimo 1): ")
 
-print("\nSoma dos dados é: " + str(soma))
+    print("\nTotal de dado(s) a rolar: " + totalDados)
+
+    for x in range(0, int(totalDados)):
+        valor = randint(1, int(lados))
+        print("\nValor do dado nº " + str(x+1) + " é " + str(valor))
+        soma = soma + valor
+
+    print("\nSoma dos dados é: " + str(soma))
+
+    while ((pergunta.upper() != "S" and pergunta.upper() != "N")) :
+        pergunta = input("Deseja rolar novos dados? (S/N): ")
+        if (pergunta.upper()) == "N":
+            continua = False
+            print("\nAté logo!!!")
+        elif (pergunta.upper()) == "S":
+            continua = True
+            break
+
+
+
