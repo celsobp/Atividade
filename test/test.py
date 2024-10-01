8d564d580b05bd43d806ce60203f2d6ae273ea5c
celsobp <celsobp@gmail.com>
2024-09-30 23:16:44 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,14 +5,12 @@ from unittest.mock import patch
 def test_lado():
     assert lado() == 6
 
-def dados():
+def test_dados():
     assert dados() == 2
 
 
-def rolar(totaldados, lados):
-    totaldados = 2
-    lados = 10
-    with patch('random.randint', return_value=7):
+def test_rolar(totaldados, lados):
+    with patch('random.randint', return_value=5):
         result = rolar(totalDados, lados)
 
-    assert result == {"teste": True, "soma": 14}
+    assert rolar == {"teste": True, "soma": 10}
