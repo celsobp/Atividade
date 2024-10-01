75b58a73df818944bc396cc33f7ffd0bb21adb3a
celsobp <celsobp@gmail.com>
2024-09-30 21:21:50 -0300

    criando-testes-unitarios

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
-ADD main.py .
+ADD src/main.py .
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
--- src/main.py
+++ src/main.py
@@ -1,24 +1,10 @@
 # Rolando dados
-from random import randint
+from funcoes import *
 
 continua = True
 
 
-def lado():
-    lados = input("Digite o número de lados dos dados (mínimo 6): ")
-    return lados
 
-def dados():
-    dado = input("\nDigite o total de dados a rolar (mínimo 1): ")
-    return dado
-
-def rolar(totalDados):
-    soma = 0
-    for x in range(0, int(totalDados)):
-        valor = randint(1, int(lados))
-        print("\nValor do dado nº " + str(x + 1) + " é " + str(valor))
-        soma = soma + valor
-    return soma
 
 while (continua) == True :
 
@@ -37,7 +23,7 @@ while (continua) == True :
 
     print("\nTotal de dado(s) a rolar: " + totalDados)
 
-    soma = rolar(totalDados)
+    soma = rolar(totalDados, lados)
 
     print("\nSoma dos dados é: " + str(soma))
 
