f6cbdf2bac21c4ec00cea1f8d7b94de77b644edd
celsobp <celsobp@gmail.com>
2024-09-30 23:22:20 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -13,4 +13,4 @@ def test_rolar(totalDados, lados):
     with patch('random.randint', return_value=5):
         result = rolar(2, 6)
 
-    assert rolar == {"teste": True, "soma": 10}
+    assert result == 10
